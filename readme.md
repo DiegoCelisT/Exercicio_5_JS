d1aec425a21b1851420986c774d795a7f815cea7
Diego Celis <diegofercelis@gmail.com>
2021-02-26 03:35:09 -0300

    Projeto estilizado, ficou MUITO irado, adorei como ficou

--- readme.md
+++ readme.md
@@ -1,5 +1,7 @@
 Vamos a praticar o uso de uma API pública.
 
-Nesse caso eu criei uma página onde você pode procurar por pokemons e receber uma foto dele e ele vai te cumprimentar!
+Nesse caso eu criei uma página onde você pode procurar por pokemons e receber uma foto dele, além disso ele vai te cumprimentar!
 
 Vai lá completar a sua pokedex!
+
+Nota: Até agora é o momento mais divertido que já tive no meu aprendizado de programação! Amei de coração cada instante brincando com isto!
--- script.js
+++ script.js
@@ -7,32 +7,44 @@ function Selecionando_pokemon (event){
     poke_nome = pokemon_selecionado.value;
     pokemon_selecionado.value = "";
 
-
     pokemon = `https://pokeapi.co/api/v2/pokemon/${poke_nome}`
-let Alldata
-let Nome
-let Pedaço_do_Nome
-let Grito //A coisa mais divertida que já fiz programando
+
+    let Alldata
+    let Nome
+    let Pedaço_do_Nome
+    let Grito //A coisa mais divertida que já fiz programando
 
 
+    fetch (pokemon)
+    .then (resposta => resposta.json())
+    .then (data =>{
+        Alldata = data; //Aqui vou salvar tudo por se quero ver depois, a variavel teve que ser definida antes
+        Nome = data.forms[0].name;
+        Pedaço_do_Nome = Nome.length/1.5
+        Grito = Nome.slice(0, Pedaço_do_Nome);
+        let elemento = document.getElementById ('meu_pokemon');
+        elemento.innerHTML = `
 
+        <span style="   -webkit-text-stroke: 1px black;
+                        color: rgb(255, 203, 5);
+                        font-size: 40px;"> Um ${Nome} selvagem apareceu na sua frente! 
+        </span>
 
-fetch (pokemon)
-.then (resposta => resposta.json())
-.then (data =>{
-    Alldata = data; //Aqui vou salvar tudo por se quero ver depois, a variavel teve que ser definida antes
-    Nome = data.forms[0].name;
-    Pedaço_do_Nome = Nome.length/1.5
-    Grito = Nome.slice(0, Pedaço_do_Nome);
-    let elemento = document.getElementById ('meu_pokemon');
-    elemento.innerHTML = `
-    <p>Um ${Nome} selvagem apareceu na sua frente</p>
-    <img src='${data.sprites.other.dream_world.front_default}' width=300px>
-    <p>${Grito} ${Grito}!!!</p> 
-    `
+        <div style="padding-top: 10px">
+        <img src='${data.sprites.other.dream_world.front_default}' height=300px alt="Opa! Esse pokemón ainda não está na minha pokedex :(")>
+        </div>
+        <div>
+        <span style="   color: white;
+                        line-height: 5vw;
+                        -webkit-text-stroke: 3px black;
+                        font-size: 100px;"> ${Grito} ${Grito}!!! 
+        </span>
+        </div>
+        `
+        
+    })
+    .catch (erro => console.log(erro))
     
-})
-.catch (erro => console.log(erro))
 }
 
 
